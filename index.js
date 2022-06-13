d1e13fe0bc9ab54be5918acb2c9f434fb2cc28b8
username <pratyushhota1999@gmail.com>
2022-06-13 23:16:05 +0530

    testing safe-branch

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ text: "Hii" });
+  res.json({ text: "Hello" });
 });
 
 app.listen(3000, () => console.log("Server Started..."));
